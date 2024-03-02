13351f545ea3aea895c4f83e3c1e944d9e33a312
Hadeed Naeem <hadeednaeem10@gmail.com>
2024-03-02 10:55:19 +0500

    Login&Register Backend

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Alert, Button } from 'react-native'
+import React, {useState} from 'react'
+import auth from '@react-native-firebase/auth';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const user = await auth().signInWithEmailAndPassword(email, password);
+      console.log(user);
+    } catch (error) {
+      if (error.code === 'auth/invalid-email') {
+        Alert.alert('Invalid Email', 'Please enter a valid email address');
+      }
+      if (error.code === 'auth/user-not-found') {
+        Alert.alert('User Not Found', 'Please enter a valid email address');
+      }
+      if (error.code === 'auth/wrong-password') {
+        Alert.alert('Invalid Password', 'Please enter a valid password');
+      }
+      console.log(error);
+    }
+  };
+
   return (
     <View>
       <Text>Login</Text>
+      <Button
+        title="Login"
+        onPress={handleLogin}
+      />
     </View>
   )
 }
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Alert, Button } from 'react-native'
+import React, {useState} from 'react'
+import auth from '@react-native-firebase/auth';
+import { firebase } from '@react-native-firebase/database';
 
 export default function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegistration = async () => {
+    try {
+      const userCredential = await auth().createUserWithEmailAndPassword(
+        email,
+        password,
+      );
+      const user = userCredential.user;
+      console.log('User registration successful:', user);
+      // Store user data in Firebase Realtime Database
+      await firebase
+        .database()
+        .ref('users/' + user.uid)
+        .set({
+          name,
+          email,
+          contact,
+          country: selectedCountry,
+          city: selectedCity,
+        });
+      console.log('User data stored in Firebase Realtime Database');
+    } catch (error) {
+      console.error('Error registering user:', error);
+      Alert.alert('Error registering user:', error.message);
+    }
+  }
+
   return (
     <View>
       <Text>Signup</Text>
+      <Button
+        title="Register"
+        onPress={handleRegistration}
+      />
     </View>
   )
 }
