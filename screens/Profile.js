da63438f6b94eca3c5e7c237e7a64a91038b51f2
Hafsakhalid-bug <leo.hafsakhalid@gmail.com>
2024-03-02 17:36:12 +0500

    userProfile UI

--- screens/Profile.js
+++ screens/Profile.js
@@ -4,93 +4,68 @@ import {
   Image,
   ImageBackground,
   TouchableOpacity,
-  ScrollView, StyleSheet
+  ScrollView,
+  StyleSheet,
 } from 'react-native';
-import React,{useState} from 'react';
+import React, {useState} from 'react';
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from 'react-native-responsive-dimensions';
 
 export default function Profile({navigation}) {
   const UserData = [
     {
       id: 1,
-      name: 'Name',
-      image: require('../assets/icons/shopping.png'),
+      name: 'Invite Friends',
+      image: require('../assets/icons/add-user.png'),
     },
     {
       id: 2,
-      name: 'Birthday',
-      image: require('../assets/icons/shopping.png'),
+      name: 'Settings',
+      image: require('../assets/icons/settings.png'),
     },
     {
       id: 3,
-      name: 'Email',
-      image: require('../assets/icons/shopping.png'),
-    },
-    {
-      id: 4,
-      name: 'Contact',
-      image: require('../assets/icons/shopping.png'),
-    },
-    {
-      id: 5,
-      name: 'Password',
-      image: require('../assets/icons/shopping.png'),
-    },
-    {
-      id: 6,
-      name: 'Donation History',
-      image: require('../assets/icons/shopping.png'),
+      name: 'Customer Support',
+      image: require('../assets/icons/customer-service.png'),
     },
   ];
 
   return (
     <ScrollView contentContainerStyle={{flexGrow: 1}}>
-      <View style={{flex:1}}>
+      <View style={{flex: 1}}>
         <View style={{width: '100%'}}>
-          <Image
+          {/* <Image
             source={require('../assets/icons/shopping.png')}
             resizeMode="cover"
             style={styles.gradientBackground}
-          />
+          /> */}
+          <View style={styles.gradientBackground}></View>
           <View style={{alignItems: 'center'}}>
-            <View
-              style={styles.imageView}>
+            <View style={styles.imageView}>
               <Image
-                source={require('../assets/icons/shopping.png')}
+                source={require('../assets/icons/baji.png')}
                 style={styles.userImage}
               />
             </View>
             <Text style={styles.userName}>Hafsa Khalid</Text>
           </View>
         </View>
-        <View
-          style={styles.contentView}>
+        <View style={styles.contentView}>
           {UserData.map(item => (
             <TouchableOpacity key={item.id}>
-              <View
-                key={item.id}
-                style={styles.listItem}>
+              <View key={item.id} style={styles.listItem}>
                 <Image
                   source={item.image}
-                  resizeMode="contain"
+                  resizeMode='center'
                   style={styles.itemIcon}
                 />
-                <Text
-                  style={styles.itemText}>
-                  {item.name}
-                </Text>
+                <Text style={styles.itemText}>{item.name}</Text>
               </View>
             </TouchableOpacity>
           ))}
-        </View>
-        <View style={{margin:30}}>
-          <TouchableOpacity  style={{alignItems:'center',justifyContent:'center'}}>
-            <Image
-              source={require('../assets/icons/shopping.png')}
-              resizeMode="cover"
-              style={styles.btnBackground}
-            />
-            <Text style={styles.btnText}>Edit Profile</Text>
-          </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
@@ -98,16 +73,17 @@ export default function Profile({navigation}) {
 }
 
 const styles = StyleSheet.create({
-gradientBackground:{
-  height: 600,
-  width: 500,
-  borderRadius: 999,
-  marginLeft: -50,
-  marginTop: -380,
+  gradientBackground: {
+    height: responsiveHeight(70),
+    width: responsiveWidth(150),
+    borderRadius: 500,
+    marginLeft: -80,
+    marginTop: -250,
+    backgroundColor: 'rgba(57, 40, 0, 1)',
   },
-  imageView:{
-    borderColor: 'grey',
-    borderWidth: 2,
+  imageView: {
+    // borderColor: 'grey',
+    // borderWidth: 2,
     width: 120,
     height: 120,
     borderRadius: 100,
@@ -118,47 +94,58 @@ gradientBackground:{
     paddingBottom: 8,
     backgroundColor: 'white',
   },
-  userImage:{
-    width: 80,
-    height: 80,
-    borderWidth: 2
+  userImage: {
+    width: responsiveWidth(34),
+    height: responsiveHeight(17),
+    // borderWidth: 2,
+    borderRadius:100
   },
-userName:{
-  color: 'black',
-  marginVertical: 8,
-  fontSize: 20,
-  fontWeight: 'bold',
+  userName: {
+    color: 'rgba(0, 0, 0, 1)',
+    marginVertical: 8,
+    fontSize: responsiveFontSize(3),
+    fontWeight: 'bold',
   },
-contentView:{
-  width: '100%',
-  flex: 1,
-  height: '100%',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-},
-listItem:{
-  height: 40,
-  width: '100%',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-around',
-  borderWidth: 1,
-  marginBottom: 10,
-  borderBottomColor: 'grey',
-  borderTopColor: 'white',
+  contentView: {
+    width: responsiveWidth(100),
+    flex: 1,
+    marginTop: responsiveWidth(10),
+    // backgroundColor:'lightblue',
+    height: responsiveHeight(20),
+    flexDirection: 'column',
+    alignItems: 'center',
+    // justifyContent: 'space-between',
+  },
+  listItem: {
+    height: responsiveHeight(7),
+    width: responsiveWidth(100),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderWidth: 1,
+    // marginBottom: responsiveWidth(5),
+    borderBottomColor: 'lightgrey',
+    borderTopColor: 'white',
+    borderRightColor: 'white',
+    borderLeftColor: 'white',
   },
   itemIcon: {
-    tintColor: 'maroon',
-    marginLeft: 15,
-    marginBottom: 8, height: 35, width: 35
+    tintColor: 'rgba(57, 40, 0, 1)',
+    backgroundColor: 'rgba(233, 171, 23, 1)',
+    borderRadius:20,
+    marginLeft: -30,
+    marginRight: -80,
+    marginBottom: 3,
+    // padding:responsiveWidth(5),
+    height: responsiveHeight(5),
+    width: responsiveHeight(5),
   },
-itemText:{
-  color: 'gray',
-  fontSize: 18,
-  width: '60%',
-  justifyContent: 'flex-start',
-},
+  itemText: {
+    color: 'gray',
+    fontSize: 18,
+    width: '60%',
+    justifyContent: 'flex-start',
+  },
 
   btnBackground: {
     alignItems: 'center',
@@ -170,7 +157,6 @@ itemText:{
   },
   btnText: {
     color: 'white',
-    fontSize: 18
+    fontSize: 18,
   },
-})
-
+});
